be4800b22592aab3e948a983401c62d5ccb833e6
Santiago <alexander.cardenas10@outlook.com>
2022-09-13 11:27:58 -0500

    Create greetings table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.destroy_all
+Greeting.create(message: "Hello world!1")
+Greeting.create(message: "Hello world!2")
+Greeting.create(message: "Hello world!3")
+Greeting.create(message: "Hello world!4")
+Greeting.create(message: "Hello world!5")
+
